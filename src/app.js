dd40708852c0e57e112460ed7e2c4867200c279d
Howard.Zuo <leftstick@qq.com>
2015-11-04 13:46:14 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,71 @@
+/**
+ * The reveal-slider
+ *
+ * The speaker can use this plugin to control their reveal slides.
+ */
+var UI = require('ui');
+var Vibe = require('ui/vibe');
+var Vector2 = require('vector2');
+var Settings = require('settings');
+
+Settings.config(
+  { url: 'http://leftstick.gitcafe.io' },
+  function(e) {
+    console.log('opening configurable');
+
+    // Reset color to red before opening the webview
+    Settings.option('color', 'red');
+  },
+  function(e) {
+    console.log('closed configurable');
+  }
+);
+
+var wind = new UI.Window();
+
+var title = new UI.Text({
+    position: new Vector2(0, 0),
+    size: new Vector2(144, 168),
+    font: 'gothic-24-bold',
+    text: 'Reveal Slider',
+    color: 'black',
+    backgroundColor: 'white',
+    textAlign: 'center'
+});
+wind.add(title);
+
+var subTitle = new UI.Text({
+    position: new Vector2(0, 30),
+    size: new Vector2(144, 168),
+    font: 'gothic-18',
+    text: 'current position',
+    color: 'black',
+    backgroundColor: 'white',
+    textAlign: 'center'
+});
+wind.add(subTitle);
+
+var slideShow = new UI.Text({
+    position: new Vector2(0, 60),
+    size: new Vector2(144, 168),
+    font: 'gothic-24',
+    text: '1 - 1',
+    color: 'black',
+    backgroundColor: 'white',
+    textAlign: 'center'
+});
+wind.add(slideShow);
+
+wind.show();
+
+wind.on('click', 'up', function() {
+  console.log('Up clicked!');
+});
+
+wind.on('click', 'down', function() {
+  console.log('down clicked!');
+});
+
+wind.on('click', 'select', function() {
+  console.log('select clicked!');
+});
