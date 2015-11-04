28691dd297dc2bfc1ce3ffed67b5e0e7abc031f8
Howard.Zuo <leftstick@qq.com>
2015-11-04 15:11:40 +0800

    set init option

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 (function(window, document) {
     'use strict';
 
+    document.querySelector('#remoteAddress').value = localStorage.remoteAddress;
+
     document.querySelector('#submit').addEventListener('click', function() {
         var options = {
             remoteAddress: document.querySelector('#remoteAddress').value
         };
 
-        window.location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options));;
+        window.location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options));
     }, false);
 
 }(window, document));
