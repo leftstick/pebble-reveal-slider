cab70d4f69d35071e688d41bf1ccc09db319fc3b
Howard.Zuo <leftstick@qq.com>
2015-11-04 17:19:37 +0800

    update configuration web

--- js/main.js
+++ js/main.js
@@ -1,14 +1,43 @@
 (function(window, document) {
     'use strict';
 
-    document.querySelector('#remoteAddress').value = localStorage['remoteAddress'];
+    var getConfigData = function () {
+        var address = document.getElementById('address');
 
-    document.querySelector('#submit').addEventListener('click', function() {
         var options = {
-            remoteAddress: document.querySelector('#remoteAddress').value
+            'background_color': address.value
         };
+        // Save for next launch
+        localStorage['address'] = options['address'];
+        console.log('Got options: ' + JSON.stringify(options));
+        return options;
+    };
 
-        window.location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options));
-    }, false);
+    var getQueryParam = function (variable, defaultValue) {
+        var query = location.search.substring(1);
+        var vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            if (pair[0] === variable) {
+                return decodeURIComponent(pair[1]);
+            }
+        }
+        return defaultValue || false;
+    };
+
+    var submitButton = document.getElementById('submit_button');
+    submitButton.addEventListener('click', function() {
+        console.log('Submit');
+        // Set the return URL depending on the runtime environment
+        var return_to = getQueryParam('return_to', 'pebblejs://close#');
+        document.location = return_to + encodeURIComponent(JSON.stringify(getConfigData()));
+    });
+
+
+    var address = document.getElementById('address');
+    // Load any previously saved configuration, if available
+    if(localStorage['address']) {
+        address.value = localStorage['address'];
+    }
 
 }(window, document));
